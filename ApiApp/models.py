fcde3a69ebe1f6fb9b10834670d0c964b2a31827
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-03-24 13:41:55 +0530

    Create models.py and serializers.py

--- ApiApp/models.py
+++ ApiApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#  Company model
+class Companny(models.Model):
+    company_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    location=models.CharField( max_length=50)
+    about=models.TextField()
+    type=models.CharField(max_length=100,choices=(('IT','IT'),('Non IT','Non IT'),('Mobiles Phones','Mobile Phones')))
+                    
+    added_date=models.DateTimeField( auto_now=True)
+    active=models.BooleanField(default=True)
